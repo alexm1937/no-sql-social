f91f3a4267fe271f40ea406ca854bbf10702c31e
unknown <alexm1397@gmail.com>
2022-01-01 12:16:39 -0600

    finished thought model and created reaction as subdocument

--- models/Thought.js
+++ models/Thought.js
@@ -1,19 +1,37 @@
 
 const { Schema, model } = require('mongoose');
+const formatDate = require('../utils/formatDate');
 
+//schema for subdoc Reaction
+const ReactionSchema = new Schema(
+    {
+        reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+        reactionBody: { type: String, required: true, minlength: 1, maxlength: 280 },
+        username: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now, get: (createdAtVal) => formatDate(createdAtVal) }
+    }
+);
+
+
+//schema for thoughts. Reactions are subdocs of Thought
 const ThoughtSchema = new Schema(
     {
         thoughtText : {type: String, required: true, minlength: 1, maxlength: 280 },
-        createdAt: {},
-        username: { type: Schema.Types.ObjectId, ref: 'User'}//use populate in route to fill in!
-        // reactions: array of subdocs (reactionSchema)
+        createdAt: { type: Date, default: Date.now, get: (createdAtVal) => formatDate(createdAtVal) },
+        username: { type: Schema.Types.ObjectId, ref: 'User', required: true },//use populate in route to fill in!
+        reactions: [ReactionSchema]
     },
     {
-        toJSON: { virtuals: true, getters: true }
+        toJSON: { virtuals: true, getters: true },
+        // id: false
     }
-)
+);
 
 //virtual 'reactionCount' 
+ThoughtSchema.virtual('ReactionCount').get(function() {
+    return this.reactions.length;
+});
+
 
 const Thought = model('Thought', ThoughtSchema);
 
